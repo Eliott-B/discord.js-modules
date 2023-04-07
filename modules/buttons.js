ec5486a61c719da4ad229817580c3d75d65bac92
Eliott-B <eliottb.info@gmail.com>
2023-04-07 07:40:21 +0200

    🖲️ | Button module

---
+++ modules/buttons.js
@@ -0,0 +1,18 @@
+const { ButtonBuilder } = require('discord.js');
+
+module.exports = (id, label, style, link, disabled) => {
+    if(link != null){
+        var button = new ButtonBuilder()
+                    .setLabel(label)
+                    .setStyle(style)
+                    .setURL(link)
+                    .setDisabled(disabled)
+    }else{
+        var button = new ButtonBuilder()
+                    .setCustomId(id)
+                    .setLabel(label)
+                    .setStyle(style)
+                    .setDisabled(disabled)
+    }
+    return(button)
+};
